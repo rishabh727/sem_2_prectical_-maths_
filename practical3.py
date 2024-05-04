388b053ea6abd8f94fec1a606e129a3d3a60c979
rishabh727 <143151167+rishabh727@users.noreply.github.com>
2024-05-04 22:04:10 +0530

    Update practical3.py

--- practical3.py
+++ practical3.py
@@ -1,14 +1,13 @@
-# practical by Rishabh
-"""3. Write a Program that generates all the permutations of a given set of Set, with or 
-without repetition."""
-from itertools import permutations,product
+from itertools import permutations, product
+
 def generate_permutations(Set, repetition):
     if repetition:
         return list(permutations(Set))
     else:
         return list(product(Set, repeat=len(Set)))
+
 if __name__ == "__main__":
-    Set = set(map(int, input("Enter all element of set with space:").split()))
+    Set = set(map(int, input("Enter all elements of set with space: ").split()))
     with_repetition = generate_permutations(Set, repetition=True)
     without_repetition = generate_permutations(Set, repetition=False)
     print("Permutations with repetition:")
@@ -16,4 +15,4 @@ if __name__ == "__main__":
         print(perm)
     print("\nPermutations without repetition:")
     for perm in without_repetition:
-        print(len(with_repetition),len(without_repetition))
+        print(perm)
